8f7a62d8fc4624530ec653da19784afdac14eb5a
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-06-26 16:45:05 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,14 @@
-window.location.href = '/api/login';
+const { parse } = require('cookie');
+
+module.exports = async (req, res) => {
+    const cookies = parse(req.headers.cookie || '');
+    
+    // Si no tiene la cookie 'uid', redirige al login
+    if (!cookies.uid) {
+        return res.redirect('/api/login');
+    }
+
+    // Si estás aquí, el usuario está autenticado. 
+    // Para redirigir a la raíz desde el servidor usa esto:
+    return res.redirect('/');
+};
