d0ecdfee2954fc170846d9528a77a0301dfaae1b
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-06-26 14:32:00 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,16 @@
-
 <link rel="stylesheet" href="/index.css"/>
-<div class="login-container">
-  <div class="card">
-    <div class="logo">
-      <img src="URL_DE_TU_IMAGEN_AQUI" alt="Logo Bombers">
-    </div>
-    <h1>ACCESO - BOMBERS CATRP</h1>
-    <p>Por favor, autentícate para continuar al sistema interno.</p>
+
+
+<!-- Estructura HTML -->
+<div class="login-wrapper">
+  <div class="login-box">
+    <img src="URL_DE_TU_IMAGEN_AQUI" alt="Logo Bombers" class="logo">
+    <h2>ACCESO - BOMBERS CATRP</h2>
+    <p>Acceso restringido a personal autorizado.</p>
     
-    <a href="/login/discord" class="discord-btn">
-      <i class="fab fa-discord"></i> Conectar con Discord
+    <a href="/login/discord" class="btn-discord">
+      <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png" alt="Discord">
+      Acceder con Discord
     </a>
   </div>
 </div>
