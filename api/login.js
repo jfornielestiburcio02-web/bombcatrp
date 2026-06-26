fa88dc0a7fbcfca8ab2442e2f1c13fc133520566
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-06-26 16:27:23 +0200

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,6 @@
+module.exports = (req, res) => {
+    const client_id = '1519766493070495844'; // Pon aquí tu Client ID
+    const redirect_uri = 'https://bomberscatrp.vercel.app/api/callback';
+    const url = `https://discord.com/api/oauth2/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`;
+    res.redirect(url);
+};
