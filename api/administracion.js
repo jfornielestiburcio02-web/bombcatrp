165eca726ff153c2ea35aa5ba25cf960a98af6d0
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-07-27 15:32:13 +0200

    Update administracion.js

--- api/administracion.js
+++ api/administracion.js
@@ -32,6 +32,7 @@ module.exports = async (req, res) => {
             <a href="/api/admin/ponFal" style="padding:15px; background:#2c3e50; color:white; text-decoration:none;">Sancionar a un usuario</a>
             <a href="/api/admin/eliPonInfo" style="padding:15px; background:#2c3e50; color:white; text-decoration:none;">Informes (Visualizar y eliminar)</a>
             <a href="/api/admin/comunicAdos" style="padding:15px; background:#2c3e50; color:white; text-decoration:none;">Comunicados desde la plataforma</a>
+            <a href="/api/admin/verServiciosTodos" style="padding:15px; background:#2c3e50; color:white; text-decoration:none;"> Servicios, tasas y resúmenes</a>
             <a href="/api/admin/otros" style="padding:15px; background:#2c3e50; color:white; text-decoration:none;">Otras opciones</a>
             <br>
             <a href="/api/acceso" style="padding:15px; background:#7f8c8d; color:white; text-decoration:none;">Volver al panel general</a>
